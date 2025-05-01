6f17e1a4dc23ede7ce6bcb8a6d6e33fa9a499af8
Hussean Kalache <husseankalache@hotmail.com>
2025-05-01 16:00:56 +1000

    Commiting testing files

--- test_blob.py
+++ test_blob.py
@@ -6,6 +6,7 @@ from blob import Blob
 
 #Unittesting for my blob-class
 class TestBlob(unittest.TestCase):
+    os.environ["BASE_DIR"] = "test_blobs"
     #just setting a random num str at the end of a test document, to add randomness
     random_num = str(randrange(0, 300))
     #uses the hook method setUp to create a new test.txt file
@@ -24,10 +25,12 @@ class TestBlob(unittest.TestCase):
         #create and store blob
         blob = Blob(self.test_file)
         blob_path = blob.store()
-        #load
+
+        #load, currently returns the SHA of the object
         load_blob = Blob.load(blob_path)
-        #assertion for unnittest
-        self.assertEqual(load_blob.data.decode(),"This is just a test document" + self.random_num)
+
+        #assertion for unnittest, reads the SHA
+        self.assertEqual(load_blob.data.decode(),("This is just a test document" + self.random_num))
 
 #Run baby run
 if __name__ == "__main__":
---
+++ test_main.py
@@ -0,0 +1,35 @@
+from main import *
+'''Testing repo/main functionality'''
+
+
+
+if __name__ == "__main__":
+    file_path = initialize_repository("test_repo")
+    #Random tests below
+    empty_test_file = os.path.join(file_path, "test_file_empty.txt")
+
+    with open(empty_test_file, "w") as f:
+        f.write("")
+
+    try:
+        #Incorrect file
+        fail_test = Blob("test3.txt")
+        fail_test.store()
+    except Exception as e:
+        print(e)
+
+
+    try:
+        #No file given
+        fail_test2 = Blob()
+    except Exception as e:
+        print(e)
+
+    try:
+        #Empty
+        empty_test_blob = Blob(empty_test_file)
+        empty_test_blob.store()
+    except Exception as e:
+        print(e)
+
+
