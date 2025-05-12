0bbd40689dee28b9f6e64289d18466df4921eb00
Hussean Kalache <husseankalache@hotmail.com>
2025-05-12 17:28:14 +1000

    Updated/Finished Tree, with unittests

---
+++ test_tree.py
@@ -0,0 +1,66 @@
+import os
+import shutil
+import unittest
+
+from tree import Tree
+from utils import decode_sha_to_path
+from repository import initialize_repository
+
+class TestTree(unittest.TestCase):
+    
+    def setUp(self):
+        self.repo_dir = "test_repo_tree"
+        if os.path.exists(self.repo_dir):
+            #destroys the path
+            shutil.rmtree(self.repo_dir)
+        
+        os.makedirs(self.repo_dir)
+        initialize_repository(self.repo_dir)
+
+        with open(os.path.join(self.repo_dir, "file1.txt"), "w") as f:
+            f.write("this is file 1")
+        
+        with open(os.path.join(self.repo_dir, "file2.txt"), "w") as f:
+            f.write("this is file 2")
+
+        nested_dir = os.path.join(self.repo_dir, "nested")
+        os.makedirs(nested_dir)
+
+        with open(os.path.join(nested_dir, "nested_file.txt"), "w") as f:
+            f.write("nested file")
+        
+    def tearDown(self):
+        shutil.rmtree(self.repo_dir)
+    
+
+    def test_tree_store_and_load(self):
+        #Create tree and store + save obj id
+        tree = Tree(self.repo_dir)
+        object_id = tree.store()
+
+        #Assert that it exists
+        self.assertIsNotNone(object_id)
+        self.assertTrue(os.path.exists(decode_sha_to_path(object_id)))
+        
+        #returns tree obj
+        loaded_tree = Tree.load(object_id)
+
+        #assert that the obj_id is the same, and that it's a tree
+        self.assertEqual(loaded_tree.object_id, object_id)
+        self.assertEqual(loaded_tree.path_type, "tree")
+
+        #2 files, 1 subdirectory
+        self.assertEqual(len(loaded_tree.children), 3)
+
+        #check each child
+        for entry in loaded_tree.children:
+            #double check that each child is a tuple()
+            self.assertEqual(len(entry), 3)
+            #Check that the first is either blob or tree
+            self.assertIn(entry[0], ("blob", "tree"))
+            #Then make sure both the other tuples are stored as strs
+            self.assertIsInstance(entry[1], str)
+            self.assertIsInstance(entry[2], str)
+
+if __name__ == "__main__":
+    unittest.main()
--- tree.py
+++ tree.py
@@ -20,7 +20,7 @@ class Tree:
         if self.tree_path is None:
             raise ValueError("Must provide a path to a project")
     
-    def store(self) -> None:
+    def store(self) -> str:
         '''
         Used to store tree objects
         '''
@@ -32,11 +32,14 @@ class Tree:
 
         #loop over all and add all children SHAs to self.children
         for each_child in sorted_dir:
-            path_type = Tree.check_path_type(os.path.join(self.tree_path, each_child))
+            if each_child == ".minigit":
+                continue
+            file_path = os.path.join(self.tree_path, each_child)
+            path_type = Tree.check_path_type(file_path)
             if path_type == "tree":
-                self.children.append(Tree.handle_tree(self.tree_path, each_child))
+                self.children.append((path_type, each_child, Tree.handle_tree(file_path)))
             elif path_type == "blob":
-                self.children.append(Tree.handle_file(self.tree_path, each_child))
+                self.children.append((path_type, each_child, Tree.handle_file(file_path)))
 
         #Reads the data and sets it to the blob-object
         self.data = self.read_child_data()
@@ -53,12 +56,16 @@ class Tree:
         return self.object_id
 
     @staticmethod
-    def check_path_type(path):
+    def check_path_type(path:str) -> str:
+        '''
+        Validates the incoming path and returns a str value either "blob" or "tree"
 
+        :Raises: FileNotFoundError if path doesn't exist
+        '''
         if os.path.isfile(path):
             validate_file(path)
             return "blob"
-        if os.path.exists(path):
+        if os.path.isdir(path):
             validate_directory(path)
             return "tree"
         
@@ -70,40 +77,81 @@ class Tree:
         '''
         Reads the data and encodes it as bytes
         '''
-
         '''
-        The data of the tree is a join of all the child object ids
-        which are stored in self.children
-        these are encoded to ascii and then changed into byte form
+        Each obj in self.children is a tuple of (entry_type, file_name, obj_id)
+        It then gets added to lines and joined. to be returned as data
         '''
-        return b"".join(child_obj_id.encode("ascii") for child_obj_id in self.children)
+        lines = []
+        for entry_type, file_name, obj_id in self.children:
+            lines.append(f"{entry_type} {file_name} {obj_id}")
+        return "\n".join(lines).encode("ascii")
 
 
 
     @staticmethod
-    def handle_file(path, child):
-        new_blob = Blob(os.path.join(path, child))
+    def handle_file(path: str) -> str:
+        '''
+        Takes in a path and creates a blob obj
+
+        **returns**(str): object id (sha256)
+        '''
+        new_blob = Blob(path)
         blob = new_blob.store()
         return blob
     
     @staticmethod
-    def handle_tree(path, child):
-        #currently recursively creating a tree for everything inside a dir
-        new_tree = Tree(os.path.join(path, child))
+    def handle_tree(path: str) -> str:
+        '''
+        Takes in a path and creates a tree obj
+
+        **returns**(str): object id (sha256)
+        '''
+        new_tree = Tree(path)
         tree = new_tree.store()
         return tree
         
 
     @classmethod
-    def load(cls, tree: "Tree"):
-        return print(f"{tree}: {tree.object_id} \n{tree.children}")
+    def load(cls, object_id: str) -> "Tree":
+        #Loads the stored tree-path and reads raw bytes
+        path = decode_sha_to_path(object_id)
+        validate_file(path)
+        with open(path, "rb") as f:
+            content = f.read()
+        
+        #Extract the header and body
+        index = content.find(b'\0')
+        raw_header = content[:index + 1]
+        header_str = raw_header[:-1].decode()
+        header_type, length = header_str.split()
+        #Make sure it's a tree
+        assert header_type == "tree", f"Expected tree, got {header_type}"
+
+        #decode data
+        body = content[index + 1:]
+        lines = body.decode("ascii").splitlines()
+        children = []
+        for line in lines:
+            entry_type, file_name, obj_id = line.strip().split()
+            children.append((entry_type, file_name, obj_id))
+        
+        #make new tree:
+        tree = cls.__new__(cls)
+        tree.object_id = object_id
+        tree.header = raw_header
+        tree.data = body
+        tree.children = children
+        tree.tree_path = None
+        tree.path_type = "tree"
+
+        return tree
     
     def __str__(self):
-        return str(self.tree_path)
+        return self.tree_path if self.tree_path else f"Tree {self.object_id}"
 
 if __name__ == "__main__":
-
-    tree_path = os.path.join(f"..\\mini_git\\test_repo")
+    
+    tree_path = os.path.join(f"..\\test_repo")
     new_tree = Tree(tree_path)
     new_tree.store()
 
